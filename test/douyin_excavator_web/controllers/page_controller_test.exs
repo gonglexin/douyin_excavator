6ff0e8d131732f2251f73025f950e33e9e69a625
Lexin Gong <gonglexin@gmail.com>
2018-06-04 15:47:16 +0800

    Fix test

--- test/douyin_excavator_web/controllers/page_controller_test.exs
+++ test/douyin_excavator_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule DouyinExcavatorWeb.PageControllerTest do
   use DouyinExcavatorWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "解析视频"
   end
 end
